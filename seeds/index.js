e09e25c81151d3cc00ee65c2827c2a25b816c8d7
PDochev <plamendochev0@gmail.com>
2023-05-23 13:51:53 +0300

    basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod asperiores suscipit ratione aut. Quam laudantium, voluptate unde similique, earum quibusdam nam minima facilis labore a, eos molestias veniam. Aperiam, beatae.",
+      price,
     });
     await camp.save();
   }
